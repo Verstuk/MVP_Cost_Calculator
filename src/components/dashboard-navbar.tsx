b0ab06228a0d334643f2dfe98bb0625d9b31b977
Verstuk <muravin@bk.ru>
2025-04-11 08:22:50 +0000

    Крошки

--- src/components/dashboard-navbar.tsx
+++ src/components/dashboard-navbar.tsx
@@ -56,8 +56,8 @@ export default function DashboardNavbar() {
     <nav className="w-full border-b border-gray-200 bg-white py-4">
       <div className="container mx-auto px-4 flex justify-between items-center">
         <div className="flex items-center gap-4">
-          <Link href="/" prefetch className="text-xl font-bold">
-            Logo
+          <Link href="/" prefetch className="text-4xl font-bold">
+            UMA
           </Link>
         </div>
         <div className="flex gap-4 items-center">
